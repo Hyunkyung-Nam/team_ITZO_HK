0733c111598655942ae630444878301a1df0b162
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-02-21 20:02:29 +0900

    footer 모듈화

--- public/js/header.js
+++ public/js/header.js
@@ -1,23 +1,3 @@
-//함수를 전역객체에 직접 할당
-//html에서 src가 module이므로.
-window.goRelation2 = function () {
-    let selectElement = document.getElementById('relation2');
-    let selectedOption = selectElement.value;
-
-    if (selectedOption) {
-        window.open(selectedOption, '_blank');
-    }
-    console.log(selectedOption);
-};
-window.goRelation = function () {
-    let selectElement = document.getElementById('relation');
-    let selectedOption = selectElement.value;
-
-    if (selectedOption) {
-        window.open(selectedOption, '_blank');
-    }
-};
-
 $('.nav')
     .children('div')
     .children()
@@ -35,13 +15,7 @@ $('.nav')
 $('.logo-click').click(function () {
     localStorage.setItem('page', 'home_recommand');
 });
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 200) {
-        $('.btn_top_move').fadeIn();
-    } else {
-        $('.btn_top_move').fadeOut();
-    }
-});
+
 $(document).ready(function () {
     $('.nav').children().removeClass('seledted');
     if (localStorage.getItem('page') === 'map_recommand') {
